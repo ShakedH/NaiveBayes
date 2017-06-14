70338cdbcffaa7fbd5bc32fdf5c88a21bd88d7ad
שקד חזון <hazons@post.bgu.ac.il>
2017-06-14 20:52:54 +0300

    Cleaning Categorial

--- Data.py
+++ Data.py
@@ -1,9 +1,26 @@
 import pandas
+
+
 class Data:
-    Attributes = {}
+    def __init__(self, data, attributes):
+        self.data = data
+        self.attributes = attributes
+        self.numOfRecords = len(data.index)
+        self.cleanData()
 
-    def __init__(self):
-        pass
+    def cleanCategorialAttr(self, attrName):
+        mode = self.data.mode()[attrName][0]
+        self.data[attrName] = self.data[attrName].fillna(mode)
 
-testData = pandas.DataFrame.from_csv("C:\Users\user\Desktop\\test.csv")
+    def cleanData(self):
+        for attrName in self.attributes:
+            if not self.attributes[attrName]:
+                self.cleanNumericalAttr(attrName)
+            else:
+                self.cleanCategorialAttr(attrName)
 
+
+testData = pandas.DataFrame.from_csv("C:\\Users\\user\\Desktop\\train.csv")
+attrs = {'loan': ['yes', 'no']}
+dataobj = Data(testData, attrs)
+print dataobj
