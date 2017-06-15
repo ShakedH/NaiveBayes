fc03ae54f9da6ef3faf96e0bb374612695a3ede5
שקד חזון <hazons@post.bgu.ac.il>
2017-06-15 12:52:38 +0300

    Cleaning numeric

--- Data.py
+++ Data.py
@@ -7,12 +7,14 @@ class Data:
         self.attributes = attributes
         self.numOfRecords = len(data.index)
         self.cleanData()
+        self.initializeMembers()
 
     def cleanCategorialAttr(self, attrName):
         mode = self.data.mode()[attrName][0]
         self.data[attrName] = self.data[attrName].fillna(mode)
 
     def cleanNumericalAttr(self, attrName):
+        # Replace missing values with the mean value of all observations in the same class
         for classValue in self.attributes['class']:
             mean = self.data.loc[(self.data['class'] == classValue), attrName].mean()
             self.data.loc[(self.data["class"] == classValue) & (self.data[attrName].isnull()), attrName] = mean
@@ -23,7 +25,6 @@ class Data:
                 self.cleanNumericalAttr(attrName)
             else:
                 self.cleanCategorialAttr(attrName)
-
 
 testData = pandas.DataFrame.from_csv("C:\\Users\\user\\Desktop\\train.csv")
 attrs = {'balance': [], 'class': ['yes', 'no']}
