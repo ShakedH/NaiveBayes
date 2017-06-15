ee0e79ad1fef7864ea93a8e454a8fcf7d5f189ba
שקד חזון <hazons@post.bgu.ac.il>
2017-06-15 17:08:00 +0300

    Small Syntax Changes

--- Tests.py
+++ Tests.py
@@ -5,5 +5,5 @@ attrsPath = "C:\\Users\\user\\Desktop\\Structure.txt"
 trainData = pandas.DataFrame.from_csv("C:\\Users\\user\\Desktop\\train.csv", index_col=None)
 attrs = getAttributesDictionary(attrsPath)
 myData = Data(trainData=trainData, attributes=attrs, numOfBins=2)
-test = myData.numberOfRecordsByClassAndAttribute(classVal='yes', attrName='job', attrVal='management')
+test = myData.numberOfRecordsByClassAndAttribute(classVal='no', attrName='job', attrVal='management')
 print test
