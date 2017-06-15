017691c0e466ae29c59663c302e57f13f2f0e770
שקד חזון <hazons@post.bgu.ac.il>
2017-06-15 18:09:44 +0300

    Removed m from data

--- Tests.py
+++ Tests.py
@@ -1,10 +1,12 @@
 from View import getAttributesDictionary
 from Data import *
+from Classifier import *
 
-path = "C:\\Users\\Ron Michaeli\\Desktop\\"
+path = "C:\\Users\\user\\Desktop\\"
+# path = "C:\\Users\\Ron Michaeli\\Desktop\\"
 attrsPath = path + "Structure.txt"
 attrs = getAttributesDictionary(attrsPath)
 trainData = pandas.DataFrame.from_csv(path + "train.csv", index_col=None)
 myData = Data(trainData=trainData, attributes=attrs, numOfBins=2)
-test = myData.numberOfRecordsByClassAndAttribute(classVal='no', attrName='job', attrVal='management')
-print test
+classify = Classifier(data=myData)
+testData = pandas.DataFrame.from_csv(path + "test.csv", index_col=None)
