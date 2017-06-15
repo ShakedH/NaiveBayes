08bc061f23d8cd3ba7d29e315d333dc5c5d181c0
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-06-15 12:48:20 +0300

    getAttrDict function added

--- Data.py
+++ Data.py
@@ -2,11 +2,24 @@ import pandas
 
 
 class Data:
-    def __init__(self, data, attributes):
+    def __init__(self, data, attributes, numOfBins):
         self.data = data
         self.attributes = attributes
         self.numOfRecords = len(data.index)
+        self.numOfBins = numOfBins
         self.cleanData()
+
+    def discretizateAttr(self, attrName):
+        minValue = self.data[attrName].min
+        maxValue = self.data[attrName].max
+        binWidth = (maxValue - minValue) / self.numOfBins
+        binLimit = minValue + binWidth
+        bins = []
+        while binLimit <= maxValue:
+            bins.append(binLimit)
+            binLimit = min(maxValue, binLimit + binWidth)
+        self.numericAttrBins[attrName] = bins
+
 
     def cleanCategorialAttr(self, attrName):
         mode = self.data.mode()[attrName][0]
