49851d6dcb3a7468c109eeff1c6211d7a81db5be
Ron Michaeli <ronmicha@post.bgu.ac.il>
2017-06-17 10:29:07 +0300

    Development done. Testing, Testing, Testing...

--- View.py
+++ View.py
@@ -8,7 +8,7 @@ import pandas
 
 root = Tk()
 width = 700
-height = 500
+height = 300
 root.geometry('{}x{}'.format(width, height))
 root.resizable(width=False, height=False)
 
@@ -22,29 +22,31 @@ class MainWindow:
         self.validBins = False
         master.title("Naive Bayes Classifier")
 
-        gridSize = 20
-        for i in range(0, gridSize):
-            self.master.grid_columnconfigure(i, weight=(width / gridSize), uniform="tk")
-            self.master.grid_rowconfigure(i, weight=(height / gridSize), uniform="tk")
+        numOfRows = 10
+        for i in range(0, numOfRows):
+            self.master.grid_rowconfigure(i, weight=(height / numOfRows), uniform="tk")
+        numOfCols = 20
+        for i in range(0, numOfCols):
+            self.master.grid_columnconfigure(i, weight=(width / numOfCols), uniform="tk")
 
         # Controls
         self.directoryLabel = Label(master=master, text="Directory Path:")
         self.binsLabel = Label(master=master, text="Discretization Bins:")
         self.directoryEntry = Entry(master=master, state="readonly")
-        vcmd = master.register(self.validateBins)
-        self.binsEntry = Entry(master=master, validate="key", validatecommand=(vcmd, '%P'))
+        self.binsEntry = Entry(master=master)
+        self.binsEntry.bind("<Key>", self.validateBins)
         self.browseButton = Button(master=master, text="Browse", command=self.browseClicked)
         self.buildButton = Button(master=master, text="Build", command=self.buildClicked, state=DISABLED)
         self.classifyButton = Button(master=master, text="Classify", command=self.classifyClicked, state=DISABLED)
 
         # Layout
-        self.directoryLabel.grid(row=2, column=1, columnspan=3)
-        self.directoryEntry.grid(row=2, column=4, columnspan=12, sticky=W + E)
-        self.binsLabel.grid(row=4, column=1, columnspan=3)
-        self.binsEntry.grid(row=4, column=4, columnspan=2, sticky=W + E)
-        self.browseButton.grid(row=2, column=17, columnspan=2, sticky=W + E)
-        self.buildButton.grid(row=7, column=6, columnspan=3, sticky=W + E)
-        self.classifyButton.grid(row=9, column=6, columnspan=3, sticky=W + E)
+        self.directoryLabel.grid(row=2, column=1, columnspan=2)
+        self.directoryEntry.grid(row=2, column=3, columnspan=12, sticky=W + E)
+        self.binsLabel.grid(row=4, column=1, columnspan=2)
+        self.binsEntry.grid(row=4, column=3, columnspan=2, sticky=W + E)
+        self.browseButton.grid(row=2, column=16, columnspan=2, sticky=W + E)
+        self.buildButton.grid(row=6, column=6, columnspan=3, sticky=W + E)
+        self.classifyButton.grid(row=8, column=6, columnspan=3, sticky=W + E)
 
     def browseClicked(self):
         tempPath = tkFileDialog.askdirectory(initialdir=self.directoryEntry.get())
@@ -78,21 +80,22 @@ class MainWindow:
         self.classifier.classifySet(testData, filePath=self.path)
         tkMessageBox.showinfo("Classification Completed", "Classification is done!")
 
-    def validateBins(self, numOfBins):
-        if numOfBins != "":
-            try:
-                numOfBins = int(float(numOfBins))
-                if numOfBins < 2:
-                    raise ValueError
-                self.validBins = True
-                self.numOfBins = numOfBins
-                if self.path:
-                    self.buildButton['state'] = 'normal'
-                return True
-            except:
-                self.validBins = False
-                tkMessageBox.showinfo("Invalid Bins", "Discretization bins must be an integer greater than 1")
-                return False
+    def validateBins(self, event):
+        numOfBins = event.char
+        if numOfBins not in "0123456789":
+            return
+        try:
+            numOfBins = int(float(numOfBins))
+            if numOfBins < 2:
+                raise ValueError
+            self.validBins = True
+            self.numOfBins = numOfBins
+            if self.path:
+                self.buildButton['state'] = 'normal'
+        except:
+            tkMessageBox.showinfo("Invalid Bins", "Discretization bins must be an integer greater than 1")
+            self.validBins = False
+            self.buildButton['state'] = 'disabled'
 
 
 naiveBayes = MainWindow(root)
